3e35c30d12e67db5ea06b2d2cf473e784841dcd9
Felipe C Machado <84751617+Felipaodopao@users.noreply.github.com>
2025-06-13 02:51:23 +0000

    humor e criaçao home

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path # Não precisa de 'include' se não houver urls.py nos apps
-from alemdasala import views # Importe as views do seu app alemdasala
-
+from django.urls import path 
+from alemdasala import views 
+from django.urls import path, include
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     # URLs do app alem_da_sala diretamente aqui:
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.home_view, name='home'), # A URL da sua página inicial
-    # Se você tiver outras views em alemdasala, adicione-as aqui
+    path('', views.home_view, name='home'), 
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
